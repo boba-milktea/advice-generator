1b9faec6f86c085ddbc2820c7dfeb1585ce0a52d
Catherine Lin <71657755+boba-milktea@users.noreply.github.com>
2022-08-06 12:47:45 +0200

    Added fetch method
    Added another way to make the request, No Cache.

--- app.js
+++ app.js
@@ -3,20 +3,38 @@ const adviceOutput = document.querySelector(".adviceText");
 const idSpan = document.querySelector("span");
 const dice = document.querySelector(".btn");
 
+// ==========  Using fetch and no cache 
 
 dice.addEventListener("click", async function getAdvice() {
-    const random = Math.floor(Math.random() * 200);
+
     try {
-        const res = await axios.get(`https://api.adviceslip.com/advice/${random}`); 
-        let advice = res.data.slip;
+        const res = await fetch('https://api.adviceslip.com/advice', { cache: 'no-cache' })
+            .then((res) => res.json());
+        const advice = res.slip;
         showAdvice(advice);
-        console.log(advice);
     }
     catch (e) {
-        console.log("error", e)
+        console.log("error", e);
     }
 }
 );
+
+// =========  Using axios 
+
+
+// dice.addEventListener("click", async function getAdvice() {
+//     const random = Math.floor(Math.random() * 200);
+//     try {
+//         const res = await axios.get(`https://api.adviceslip.com/advice/${random}`);
+//         let advice = res.data.slip;
+//         showAdvice(advice);
+//         // console.log(advice);
+//     }
+//     catch (e) {
+//         console.log("error", e)
+//     }
+// }
+// );
 
 const showAdvice = (advice) => {
     idSpan.innerText = advice.id;
